a543d8486326c0853431fc795ebaa7925d2a36d6
nonnakip <nonnakip@nastyfans.org>
2016-05-25 22:16:19 +0000

    assignfee: implement fee assignment
    - also add MAX_TIME environment variable for account balances
      (this is helpful for listaccounts past snapshots)
    
    Signed-off-by: nonnakip <nonnakip@nastyfans.org>

---
+++ assignfee/README.md
@@ -0,0 +1,18 @@
+# assignfee
+
+## WARNING
+
+This code is for educational purposes only. Use at your own risk.
+
+## About
+
+This tool moves funds from a account to "(FEE)" to compensate transaction fees.
+
+## Usage Example
+
+Transfer transaction fee funds for transaction "425267aab0e792bd79149a18653e4d45041eb635079f51c5549efc2d7e7e845d" from account "testaccount" to "(FEE)":
+
+```
+export ACCOUNT_ROOT=/var/lib/nastyfans/accounts
+./assignfee "testaccount" 425267aab0e792bd79149a18653e4d45041eb635079f51c5549efc2d7e7e845d
+```
---
+++ assignfee/assignfee.c
@@ -0,0 +1,33 @@
+/*
+Copyright 2016 nonnakip
+This file is part of nastyfans-suite.
+
+nastyfans-suite is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+nastyfans-suite is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with nastyfans-suite.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include "account.h"
+
+/* assignfee "toaccount" txid */
+int main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s \"toaccount\" txid\n", argv[0]);
+		return 1;
+	}
+
+	account_assignfee(argv[1], argv[2]);
+
+	return 0;
+}
